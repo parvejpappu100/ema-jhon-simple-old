83a99c8eda70160efdf2053445ab882c6b8630b3
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-03-29 17:42:50 +0600

    show data from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,9 +6,14 @@ const Cart = ({cart}) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart ){
-       totalPrice = totalPrice + product.price;
+        if(product.quantity === 0){
+            product.quantity = 1;
+        }
+       totalPrice = totalPrice + product.price * product.quantity;
        totalShipping = totalShipping + product.shipping ;
+       quantity = quantity + product.quantity;
     }
     const tax = (totalPrice * 7) / 100;
     const grandTotal = totalPrice + totalShipping + tax;
